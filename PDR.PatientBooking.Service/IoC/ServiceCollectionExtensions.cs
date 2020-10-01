0e9e1e18426c9e079e6a5a3982727d3f32189dcc
vlenivenko <vladislav.lenivenko@dataart.com>
2020-10-01 09:19:55 +0300

    Ticket #2. Improve add new booking endpoint validation logic. Comment : Validation was improved according to the requirements. Add booking code was moved from controller to the service.

--- PDR.PatientBooking.Service/IoC/ServiceCollectionExtensions.cs
+++ PDR.PatientBooking.Service/IoC/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using PDR.PatientBooking.Service.BookingServices;
+using PDR.PatientBooking.Service.BookingServices.Validation;
 using PDR.PatientBooking.Service.ClinicServices;
 using PDR.PatientBooking.Service.ClinicServices.Validation;
 using PDR.PatientBooking.Service.DoctorServices;
@@ -12,6 +14,9 @@ namespace PDR.PatientBooking.Service.IoC
     {
         public static void RegisterPatientBookingServices(this IServiceCollection collection)
         {
+            collection.AddScoped<IBookingService, BookingService>();
+            collection.AddScoped<IAddBookingRequestValidator, AddBookingRequestValidator>();
+
             collection.AddScoped<IPatientService, PatientService>();
             collection.AddScoped<IAddPatientRequestValidator, AddPatientRequestValidator>();
 
--- PDR.PatientBookingApi/Controllers/BookingController.cs
+++ PDR.PatientBookingApi/Controllers/BookingController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PDR.PatientBooking.Data;
 using PDR.PatientBooking.Data.Models;
+using PDR.PatientBooking.Service.BookingServices;
+using PDR.PatientBooking.Service.BookingServices.Requests;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +15,12 @@ namespace PDR.PatientBookingApi.Controllers
     {
         private readonly PatientBookingContext _context;
 
-        public BookingController(PatientBookingContext context)
+        private readonly IBookingService _bookingService;
+
+        public BookingController(PatientBookingContext context, IBookingService bookingService)
         {
             _context = context;
+            _bookingService = bookingService;
         }
 
         [HttpGet("patient/{identificationNumber}/next")]
@@ -49,42 +54,21 @@ namespace PDR.PatientBookingApi.Controllers
         }
 
         [HttpPost()]
-        public IActionResult AddBooking(NewBooking newBooking)
+        public IActionResult AddBooking(AddBookingRequest request)
         {
-            var bookingId = new Guid();
-            var bookingStartTime = newBooking.StartTime;
-            var bookingEndTime = newBooking.EndTime;
-            var bookingPatientId = newBooking.PatientId;
-            var bookingPatient = _context.Patient.FirstOrDefault(x => x.Id == newBooking.PatientId);
-            var bookingDoctorId = newBooking.DoctorId;
-            var bookingDoctor = _context.Doctor.FirstOrDefault(x => x.Id == newBooking.DoctorId);
-            var bookingSurgeryType = _context.Patient.FirstOrDefault(x => x.Id == bookingPatientId).Clinic.SurgeryType;
-
-            var myBooking = new Order
+            try
             {
-                Id = bookingId,
-                StartTime = bookingStartTime,
-                EndTime = bookingEndTime,
-                PatientId = bookingPatientId,
-                DoctorId = bookingDoctorId,
-                Patient = bookingPatient,
-                Doctor = bookingDoctor,
-                SurgeryType = (int)bookingSurgeryType
-            };
-
-            _context.Order.AddRange(new List<Order> { myBooking });
-            _context.SaveChanges();
-
-            return StatusCode(200);
-        }
-
-        public class NewBooking
-        {
-            public Guid Id { get; set; }
-            public DateTime StartTime { get; set; }
-            public DateTime EndTime { get; set; }
-            public long PatientId { get; set; }
-            public long DoctorId { get; set; }
+                _bookingService.AddBooking(request);
+                return Ok();
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
         }
 
         private static MyOrderResult UpdateLatestBooking(List<Order> bookings2, int i)
